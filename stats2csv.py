d76cacb4e7e6b5af8a4264a7cb4912babc465723
yanbc <imyanbc@gmail.com>
2023-12-04 16:30:41 +0800

    first workable version

--- stats2csv.py
+++ stats2csv.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import os
 from dataclasses import dataclass
 from datetime import datetime
 from typing import List, Mapping
@@ -55,30 +56,7 @@ def extract_metric_line(filepath: str) -> str:
         return ""
 
 
-@dataclass
-class DataPoint:
-    gpuId: int
-    name: str
-    value: int
-
-
-@dataclass
-class DataPointWithTime(DataPoint):
-    time: datetime.time
-
-
-@dataclass
-class DataPointWithDate(DataPoint):
-    date: datetime.date
-
-
-@dataclass
-class DataPointWithDateTime(DataPoint):
-    date: datetime.date
-    time: datetime.time
-
-
-def get_all_metrics(metric_line: str, file_path: str) -> Mapping[str, List[DataPoint]]:
+def get_all_metrics(metric_line: str, file_path: str) -> Mapping[str, List[int]]:
     metric_str_list = metric_line.split()
     first_elem = metric_str_list[0]
     second_elem = metric_str_list[1]
@@ -89,40 +67,59 @@ def get_all_metrics(metric_line: str, file_path: str) -> Mapping[str, List[DataP
     if first_elem == "#" and second_elem == "gpu":
         # see docs/example_dmon.txt for an example
         metrics = metric_str_list[1:]
-        datapoints = {m: [] for m in metrics}
-        for line in lines:
-            if line[0] == "#":
-                continue
-            else:
-                data = line.split()
-                for m, d in zip(metrics, data):
-                    if d == "-":
-                        datapoints[m].append(np.nan)
-                    else:
-                        datapoints[m].append(int(d))
-
     elif first_elem == "#Time" and second_elem == "gpu":
         # see docs/example_dmon_time.txt for an example
-        metrics = metric_str_list[2:]
-
+        metrics = ["Time"] + metric_str_list[1:]
     elif first_elem == "#Date" and second_elem == "gpu":
         # see docs/example_dmon_date.txt for an example
-        metrics = metric_str_list[2:]
+        metrics = ["Date"] + metric_str_list[1:]
     elif first_elem == "#Date" and second_elem == "Time":
         # see docs/example_dmon_datetime.txt for an example
-        metrics = metric_str_list[3:]
+        metrics = ["Date"] + metric_str_list[1:]
     else:
-        pass
+        return dict()
+
+    datapoints = {m: [] for m in metrics}
+    for line in lines:
+        if line[0] == "#":
+            continue
+        else:
+            data = line.split()
+            for m, d in zip(metrics, data):
+                if d == "-":
+                    datapoints[m].append(np.nan)
+                else:
+                    if m == "Date":
+                        datapoints[m].append(str2date(d))
+                    elif m == "Time":
+                        datapoints[m].append(str2time(d))
+                    else:
+                        datapoints[m].append(int(d))
 
     return datapoints
 
 
 if __name__ == "__main__":
-    _METRICS_JSON = "./metrics.json"
+    # _METRICS_JSON = "./metrics.json"
     # metrics_dict = load_json(_METRICS_JSON)
 
     args = get_args()
-    metric_line = extract_metric_line(args.dmon_output)
-    stats_dict = get_all_metrics(metric_line=metric_line, file_path=args.dmon_output)
+    dmon_output_file = args.dmon_output
+    if not os.path.isfile(dmon_output_file):
+        print(f"file {dmon_output_file} does not exist")
+    else:
+        metric_line = extract_metric_line(dmon_output_file)
+        if metric_line == "":
+            print("metric line not found")
+        else:
+            stats_dict = get_all_metrics(
+                metric_line=metric_line, file_path=dmon_output_file
+            )
 
-    df = pd.DataFrame(data=stats_dict, dtype=pd.Int64Dtype())
+            if len(stats_dict) == 0:
+                print("ill-formatted dmon output")
+            else:
+                df = pd.DataFrame(data=stats_dict)
+                csv_file = dmon_output_file.replace(".txt", ".csv")
+                df.to_csv(csv_file, index=False, na_rep="NA")
+                print(f"csv saved as {csv_file}")
